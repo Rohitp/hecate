a16d499c410159ff226046e6d3e831588b6fc701
Rohit <nirohit@gmail.com>
2025-01-07 21:51:55 +0000

    UNSTABLE, UNCOMPILABLE COMMIT - Running a few experiments with a predictive parser. Need a stable base to get back to - 2

--- src/com/babel/hecate/grammar/Expression.java
+++ src/com/babel/hecate/grammar/Expression.java
@@ -2,7 +2,7 @@ package com.babel.hecate.grammar;
 
 
 // We eventually add a visitor for each expression here. Plus the holder for accepting the visitor
-abstract class Expression {
+public abstract class Expression {
 
     interface Visitor<T> {
         T visit(BinaryExpression expression);
--- src/com/babel/hecate/parser/Parser.java
+++ src/com/babel/hecate/parser/Parser.java
@@ -1,9 +1,13 @@
 package com.babel.hecate.parser;
 
+import com.babel.hecate.grammar.Expression;
+import com.babel.hecate.grammar.LiteralExpression;
+
 import java.beans.Expression;
 import java.util.ArrayList;
 
 import com.babel.hecate.scanner.Token;
+import com.babel.hecate.scanner.TokenEnum;
 
 // Defining a parser with the following rules
 // (Also see -> https://en.wikipedia.org/wiki/LR_parser)
@@ -17,6 +21,9 @@ import com.babel.hecate.scanner.Token;
  // This is preferable also because, each production drectly translates to a function. 
  // We can continue with the visitor pattern and add a visitor for each object
  
+
+
+ // On a fundamental level this takes tokens and converts it into expressions. 
 public class Parser {
 
     private final ArrayList<Token> tokens;
@@ -27,28 +34,48 @@ public class Parser {
     }
 
 
+    // Can potentially be infinitely long 
+    // a == b == b == d and so on
     private Expression equals() {
-
+        Expression expr = comparisson();
     }
     
-    private Expression comparisson() {
 
+    private Expression comparisson() {
+        Expression expr = summations();
     }
 
     private Expression summations() {
-
+        Expression expr = multiplication();
     }
 
     private Expression multiplication() {
-
+        Expression expr = unary();
     }
 
     private Expression unary() {
-
+        Expression expr = literal();
     }
 
+    // Literal can be true, false, string, number or Nietzsche
     private Expression literal() {
+
+        // common cases
+        if(match(TokenEnum.TRUE))
+            return new LiteralExpression(true);
+        if(match(TokenEnum.FALSE))
+            return new LiteralExpression(false);
+        if(match(TokenEnum.NIETZSCHE))
+            return new LiteralExpression(null);
+        if(match(TokenEnum.NUMBER, TokenEnum.STRING))
+            return new LiteralExpression(tokens.get(ptr -1));
         
+        
+    }
+
+
+    private boolean match(TokenEnum ...token) {
+        return true;
     }
     
 }
